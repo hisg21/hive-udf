39be6c2ca0ddceff358154e8e797a6ea6eaf9e3c
sglee <sglee@sgleeMBP.local>
2014-05-22 13:26:39 +0900

    UDF description added

--- src/main/java/com/nexr/platform/hive/udf/UDFAddMonths.java
+++ src/main/java/com/nexr/platform/hive/udf/UDFAddMonths.java
@@ -1,5 +1,6 @@
 package com.nexr.platform.hive.udf;
 
+import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.UDF;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -12,6 +13,13 @@ import java.util.Date;
 /**
  * UDFAddMonths
  */
+@Description(name = "add_months",
+        value = "_FUNC_(date, months)  returns a string with yyyy-MM-dd HH:mm:ss pattern " +
+                "to a string with given pattern after calculating with months.\n",
+        extended = "Example:\n"
+                +" > SELECT add_months('2011-05-11 10:00:12', -2) FROM src LIMIT 1;\n"
+                +"2011-03-11 10:00:12\n"
+)
 public class UDFAddMonths extends UDF {
     private final SimpleDateFormat stdFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     private final Calendar calendar = Calendar.getInstance();
